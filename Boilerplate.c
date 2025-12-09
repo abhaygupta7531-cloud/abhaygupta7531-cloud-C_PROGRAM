95bb5df90a71cc008a617510fb4621b808497b4a
Abhay Gupta <abhaygupta7531@gmail.com>
2025-12-09 20:18:30 +0530

    Boilerplate code

---
+++ Boilerplate.c
@@ -0,0 +1,19 @@
+/*this is a boilerplate C program that provides a starting point for
+ *developing C comments to help you to get started.
+ *
+ * author:abhay gupta
+ * date: 9/12/2025
+ */
+ 
+ #include<stdio.h> 
+ // The line tells the compiler: "Bring the standard input/output library so i can use printf,scanf,"
+
+ int main (){
+  //This is the main function of this program
+
+ printf("Hello world");
+
+ return 0;
+ //sends the value back,0 means success,no errors
+
+}
