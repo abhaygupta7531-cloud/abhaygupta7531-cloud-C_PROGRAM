bfa115c89b29eb418cf1b7d842e7771d6f560e13
Abhay Gupta <abhaygupta7531@gmail.com>
2025-12-10 23:15:47 +0530

    C_skeleton

---
+++ perfect_Skeleton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>   // 1. Header file
+
+int main() {         // 2. Main function starts
+
+    int a, b;        // 3. Variable declaration
+
+    a = 10;          // 4. Program body (logic)
+    b = 20;
+
+    printf("Sum = %d", a + b);
+
+    return 0;        // 5. Program ends
+}
