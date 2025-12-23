44b00fe6b1fb7aa103a678a3ced54a66920779ce
Abhay Gupta <abhaygupta7531@gmail.com>
2025-12-23 19:16:28 +0530

    practice set 01

---
+++ practice set 01/01.2_problem.c
@@ -0,0 +1,13 @@
+int main(){
+    int length,breadth;
+
+    printf("Enter length\n");
+    scanf("%d",&length);
+
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+
+    printf("THe are of rectangle is %d", length*breadth);
+
+    return 0;
+}
---
+++ practice set 01/01_problem.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int length = 5;
+    int breadth = 12;
+
+    printf("THe are of rectangle is %d", length*breadth);
+
+    return 0;
+}
