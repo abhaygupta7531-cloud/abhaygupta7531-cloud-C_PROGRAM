7411dff22345513d51a8c6a8a6e526caf6f3c498
Abhay Gupta <abhaygupta7531@gmail.com>
2025-12-23 18:50:44 +0530

    04_escape_sequence.c

---
+++ 03_input.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a); 
+    /* To taking input from user 
+    thearfor we use scanf function and 
+    2) & is a address of operator
+    3) for taking integer we take %d
+    4) for taking float we take   %f
+    5) for taking character we take %c 
+       Syntax: scanf("%d",&a)
+    */
+    printf("The value of a is %d",a);
+    return 0;
+
+
+}
---
+++ 04_escape_sequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    char ch = 'a';
+    ch = 'c';
+    ch = '\n'; // represents a new line
+    printf("hey I am good \" nice");
+    printf("hey how are you \t sam ");
+    printf("who are you \ boy ");
+    return 0;
+
+
+}
