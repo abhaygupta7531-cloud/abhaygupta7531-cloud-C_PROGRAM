14c246e89abcbc34558c91ff40d2cff2f071c342
Abhay Gupta <abhaygupta7531@gmail.com>
2026-01-01 12:58:30 +0530

    chapter 2

---
+++ chapter 2: Instructions and Operators/01_variabless.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){   
+    int i = 10; //Declare and initialize "i" with 10
+    int j = i;  //Declare "j" and initialize with "i"
+    int a = 2, b = 3, c = 4, d = 5; //Declare and initialize multiple variables
+    // %d is called format specifier
+    // %d is for int,%f is for float and %c is for char
+    printf("The value of i is %d and value of j is %d\n",i,j);
+    printf("The value of a is %d and the value of b is %d\n",a,b);
+    printf("The value of c is %d and the value of d is %d\n",c,d);
+    
+
+
+    return 0;
+}
---
+++ chapter 2: Instructions and Operators/arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 4;
+    int b = 6;
+    int c = a+b;
+
+    printf("The value of a is %d and the value of b is %d and sum of both %d\n",a,b,c);
+    printf("The remainder whan a is divided by b is:%d\n",a%b);
+    // Modulus operator is used to get the remainder
+    return 0; 
+}
