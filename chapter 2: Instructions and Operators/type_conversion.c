3e4a4900f9ab588069e970c395d3cd69f3005256
Abhay Gupta <abhaygupta7531@gmail.com>
2026-01-11 10:22:51 +0530

    g

---
+++ chapter 2: Instructions and Operators/type_conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 11;
+    int b = 5;
+    
+    printf("The value A is:%d\n",a==b);
+     printf("The value B is:%d\n",a!=b);
+      printf("The value H is:%d\n",a<b);
+       printf("The value A is:%d\n",a>b);
+        printf("The value Y is:%d\n",a<=b);
+    return 0;
+}
