c8d7f14907256145201f72092cb211e4e652cb1a
Abhay Gupta <abhaygupta7531@gmail.com>
2026-01-25 00:14:55 +0530

    type conversion

---
+++ chapter 2: Instructions and Operators/02type_conversion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    float age = 11.0;
+    float hight = 5.8 ;
+    printf("this man is fit age is:%.2f\n,and hight :%.2f\n",age,hight);
+    
+    return 0;
+}
--- chapter 2: Instructions and Operators/arithmetic.c
+++ chapter 2: Instructions and Operators/arithmetic.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int a = 5, b = 10;   // Declare and initialize variables
 
-    int a = 4;
-    int b = 6;
-    int c = a+b;
+    // Checks whether a is equal to b
+    // Returns 1 if true, otherwise 0
+    printf("The value A is: %d\n", a == b);
 
-    printf("The value of a is %d and the value of b is %d and sum of both %d\n",a,b,c);
-    printf("The remainder whan a is divided by b is:%d\n",a%b);
+    // Checks whether a is NOT equal to b
+    printf("The value B is: %d\n", a != b);
+
+    // Checks whether a is less than b
+    printf("The value H is: %d\n", a < b);
+
+    // Checks whether a is greater than b
+    printf("The value A is: %d\n", a > b);
+
+    // Checks whether a is less than or equal to b
+    printf("The value Y is: %d\n", a <= b);
+
+    return 0;   // End of program
+
+   // printf("The value of a is %d and the value of b is %d and sum of both %d\n",a,b,c);
+    //printf("The remainder whan a is divided by b is:%d\n",a%b);
     // Modulus operator is used to get the remainder
-    return 0; 
+     
 }
