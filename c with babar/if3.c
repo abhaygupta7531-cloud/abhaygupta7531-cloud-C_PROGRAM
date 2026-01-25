d1215ec3eebb133a1f88929e6eddf82826bb1bd2
Abhay Gupta <abhaygupta7531@gmail.com>
2026-01-26 00:26:55 +0530

    v

---
+++ c with babar/if3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("The value of a is\n");
+    scanf("%d",&a);
+
+    printf("The value of b is\n");
+    scanf("%d",&b);
+    
+    if(a>b){
+        printf("papa");
+    }else{
+        printf("Don");
+    }
+
+
+    return 0;
+}
