8209658912c8e3ca8f327f3c98b2774ae9d3113c
Abhay Gupta <abhaygupta7531@gmail.com>
2026-01-26 00:18:24 +0530

    b

---
+++ c with babar/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    float n;
+
+    scanf("%f",&n);
+    
+    if(n>7){
+        printf("the value is true\n");
+    }else{
+        printf("The value is fales\n");
+    }
+
+
+
+    return 0;
+}
---
+++ c with babar/if2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b){
+        printf("The value is dog\n");
+    }else{
+        printf("The value is boss\n");
+    }
+
+
+
+
+
+    return 0;
+}
