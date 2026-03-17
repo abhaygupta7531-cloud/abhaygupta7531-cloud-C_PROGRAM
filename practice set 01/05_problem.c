ecd19140d66adbd925a18fcbbbfdf9445e578d5f
Abhay Gupta <abhaygupta7531@gmail.com>
2026-03-17 15:08:11 +0530

    h

--- practice set 01/05_problem.c
+++ practice set 01/05_problem.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    float s1, s2, s3, s4, s5;
+    float s1, s2, s3, s4, s5,s6,s7,s8,s9,s10,s11;
     float sum, percentage;
 
     // Input marks
-    printf("Enter marks of 5 subjects:\n");
-    scanf("%f %f %f %f %f", &s1, &s2, &s3, &s4, &s5);
+    printf("Enter marks of 11 subjects:\n");
+    scanf("%f %f %f %f %f %f %f %f %f %f %f", &s1, &s2, &s3, &s4, &s5,&s6,&s7,&s8,&s9,&s10,&s11);
 
     // Calculate sum
-    sum = s1 + s2 + s3 + s4 + s5;
+    sum = s1 + s2 + s3 + s4 + s5+s6+s7+s8+s9+s10+s11;
 
     // Calculate percentage (out of 500)
-    percentage = (sum / 500) * 100;
+    percentage = (sum / 550) * 100;
 
     // Output Why we use %.2f ? %.2f ka matlab hota hai: float value ko sirf 2 decimal places tak print karo
     printf("Total Marks = %.2f\n", sum);
--- practice set 01/tempCodeRunnerFile.c
+++ practice set 01/tempCodeRunnerFile.c
@@ -1,8 +1,2 @@
 
-    int length,breadth;
-
-    printf("Enter length\n");
-    scanf("%d",&length);
-
-    printf("Enter breadth\n");
-    scanf("%d", &breadth);
+    
